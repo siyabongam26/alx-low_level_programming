249f86b41702d3a3c07ce8d9e406ba88804e23ad
Siyabonga <siyabongam26@gmail.com>
2023-10-20 07:11:14 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,34 @@
-#include "lists.h"
-/**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
+# 0-print_list.c
 
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+* print_list - check the code for Holberton School students.
+* @h: name of the list
+* Return: the number of nodes.
+*/
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+   int count = 0;
 
-	nelem = 0;
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		nelem++;
-	}
-	return (nelem);
+
+   while (h)
+   {
+       if (h->str == NULL)
+       {
+           printf("[0] (nil)\n");
+       }
+       else
+       {
+       printf("[%d] %s\n", h->len, h->str);
+       }
+       count++;
+       h = h->next;
+   }
+   return (count);
 }
