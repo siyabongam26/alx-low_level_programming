4773ee17734934d7d975a7479861add04290e58e
siyabongam26 <siyabongam26@gmail.com>
2023-10-30 08:02:00 -0700

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,4 @@
-80% of storage used … If you run out, you won't have enough storage to create, edit, and upload files. Get 100 GB of storage for R 34,99 R 8,99/month for 3 months.
+80% of storage used … You can clean up space or get more storage for Drive, Gmail, and Google Photos.
 /*
  * File: 100-elf_header.c
  * Auth: Brennan D Baraban
