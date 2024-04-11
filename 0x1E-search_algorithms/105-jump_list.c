01b3072d711fbad2dcbf833150c7447dee2eb5d0
siyabongam26 <siyabongam26@gmail.com>
2024-04-11 01:03:06 -0700

    update

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,41 +1,36 @@
 #include "search_algos.h"
 
 /**
- * jump_list - Searches for a value in a sorted singly linked list
- *             of integers using jump search.
- * @list: A pointer to the head of the linked list to search.
+ * jump_list - Searches for an algorithm in a sorted singly
+ *             linked list of integers using jump search.
+ * @list: A pointer to the  head of the linked list to search.
  * @size: The number of nodes in the list.
  * @value: The value to search for.
  *
  * Return: If the value is not present or the head of the list is NULL, NULL.
- *         Otherwise, a pointer to the first node where the value is located.
+ *         else, a pointer to the first node where the value is located.
+ *
+ * Description: Prints a value every time it is compared in the list.
+ *              Uses the square root of the list size as the jump step.
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
 	size_t step, step_size;
 	listint_t *node, *jump;
 
-	/* Check if the list is NULL or empty */
 	if (list == NULL || size == 0)
 		return (NULL);
 
 	step = 0;
-
-	/* Calculate the jump step size */
 	step_size = sqrt(size);
-
 	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
 	{
-		/* Store the previous jump node */
 		node = jump;
-
 		for (step += step_size; jump->index < step; jump = jump->next)
 		{
-			/* If reached the end of the list */
 			if (jump->index + 1 == size)
 				break;
 		}
-
 		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
 	}
 
@@ -44,7 +39,6 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 
 	for (; node->index < jump->index && node->n < value; node = node->next)
 		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
-
 	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
 	return (node->n == value ? node : NULL);
